608e5bdf96da8bde95e4503083953edfdb21598b
Sivareddymedapati <sivareddymedapati@gmail.com>
2020-05-06 11:04:46 +0530

    Updates

---
+++ precache-manifest.2ae388574dbfa2668887f15a9ac246b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a123cd514eb8edaf2ac0c480375eb53b",
+    "url": "/Finance-app/index.html"
+  },
+  {
+    "revision": "a1d07d1972570318a656",
+    "url": "/Finance-app/static/css/main.a83c59c7.chunk.css"
+  },
+  {
+    "revision": "a61d55da58349697b7a4",
+    "url": "/Finance-app/static/js/2.a11d3aff.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Finance-app/static/js/2.a11d3aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d07d1972570318a656",
+    "url": "/Finance-app/static/js/main.2c47b385.chunk.js"
+  },
+  {
+    "revision": "827cafee7156fa359806",
+    "url": "/Finance-app/static/js/runtime-main.1e143308.js"
+  }
+]);
